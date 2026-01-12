761e5c016c244970eb7e8f240e85d64a0412f3ee
Julien Catala <julien.catala.bxl@gmail.com>
2026-01-12 15:39:36 +0100

    ui(component): standardize app container width and gutters

--- src/app/shared/components/app-container/app-container.component.ts
+++ src/app/shared/components/app-container/app-container.component.ts
@@ -9,7 +9,7 @@ import { Component } from '@angular/core';
     class: 'block',
   },
   template: `
-    <div class="max-w-screen-2xl mx-auto w-full px-4 md:px-6 lg:px-8">
+    <div class="app-container">
       <ng-content />
     </div>
   `,
